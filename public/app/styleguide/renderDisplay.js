2546a9eeab2638abf83bce99977a6a33705bd9d8
Olivier Bossel <olivier.bossel@gmail.com>
2017-02-03 16:37:36 +0100

    fix displays

--- public/app/styleguide/renderDisplay.js
+++ public/app/styleguide/renderDisplay.js
@@ -1,13 +1,16 @@
 const __displays = require('./displays/index');
 const __config = require('../config');
 const _merge = require('lodash/merge');
+const __fs = require('fs');
 module.exports = function renderDisplay(name, data) {
 	// additional displays
 	if (__config.styleguide.displays.length) {
 		__config.styleguide.displays.forEach(function(displaysFilePath) {
-			const additionalDisplays = require(displaysFilePath);
-			// merge into displays
-			_merge(__displays, additionalDisplays);
+			if (__fs.existsSync(process.env.PWD + '/' + displaysFilePath)) {
+				const additionalDisplays = require(process.env.PWD + '/' + displaysFilePath);
+				// merge into displays
+				_merge(__displays, additionalDisplays);
+			}
 		});
 	}
 

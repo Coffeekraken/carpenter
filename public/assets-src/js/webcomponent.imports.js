31ce711a6b71658cf2004c052f01d4322cbac163
Olivier Bossel <olivier.bossel@gmail.com>
2017-04-06 09:57:40 +0200

    add the about dialog

--- public/assets-src/js/webcomponent.imports.js
+++ public/assets-src/js/webcomponent.imports.js
@@ -4,3 +4,4 @@ import SCodemirrorComponent from 'coffeekraken-s-codemirror-component';
 import SCustomScrollbarComponent from 'coffeekraken-s-custom-scrollbar-component';
 import SRippleComponent from 'coffeekraken-s-ripple-component';
 import SActivateComponent from 'coffeekraken-s-activate-component';
+import SDialogComponent from 'coffeekraken-s-dialog-component'

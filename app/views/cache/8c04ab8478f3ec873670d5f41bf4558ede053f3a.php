96c1599522e9c7e5da2c5aa4da159bdd05f05320
Olivier Bossel <olivier.bossel@gmail.com>
2018-08-09 22:09:21 +0200

    remove cache files

--- app/views/cache/8c04ab8478f3ec873670d5f41bf4558ede053f3a.php
+++
@@ -1,2 +0,0 @@
-<h1><?php echo e($title); ?></h1>
-<?php echo $__env->make('atoms.button.button', $button, array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- app/views/cache/f4ba87aac772b37da9cc58b1af464cfe14eda459.php
+++
@@ -1,7 +0,0 @@
-<a class="btn btn--<?php echo e(isset($color) ? $color : 'default'); ?> btn--<?php echo e(isset($type) ? $type : 'default'); ?>"
-	title="<?php echo e($title); ?>"
-	target="<?php echo e(isset($target) ? $target : '_self'); ?>"
->
-	<?php echo e($label); ?>
-
-</a>
